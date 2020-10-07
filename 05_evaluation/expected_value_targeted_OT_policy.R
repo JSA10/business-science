efd670625973cfc946f2d6baad5e8d001bc28eb0
JSA10 <jerome.ahye@gmail.com>
2020-10-07 22:15:08 +0100

    Complete chapter 8 by adding sensitivity analysis to guage sensitivity of final cost parameters to the two main assumptions made in calculating attrition costs.

--- 05_evaluation/expected_value_targeted_OT_policy.R
+++ 05_evaluation/expected_value_targeted_OT_policy.R
@@ -440,7 +440,7 @@ rates_by_threshold_optimised_tbl <- rates_by_threshold_tbl %>%
             .f = partial(calculate_savings_by_threshold, data = test_tbl, h2o_model = automl_leader)
         )
     )
-
+write_rds(rates_by_threshold_optimised_tbl, "00_data/rates_by_threshold_optimised_tbl.rds")
 # purrr within map for rowwise iteration 
 # pmap_dbl - one of pmap functions that use a list (".l") of arguments
 # - returns single numeric value 
@@ -530,10 +530,268 @@ rates_by_threshold_optimised_tbl %>%
 # 2) Avg. OT Percent = 10% 
 
 # sensitivity analysis will analyse different variations of these estimates 
-# and construcut a probability heatmap to show how impact on savings estimates 
+# and construct a probability heatmap to show how impact on savings estimates 
 
-
+# PROCESS 
+# - CREATE A FUNCTION 
+# - USE PURRR TO ITERATE THROUGH 
+# Little bit differently to last time though 
 
 # 6.1 Create calculate_savings_by_threshold_2() ----
 
+# brings in the two above assumptions so we can test them with different variations
+
+data <- test_tbl
+h2o_model <- automl_leader
+# default threshold rates correspond to threshold of 0 
+# 
+calculate_savings_by_threshold_2 <- function(data, h2o_model, threshold = 0,
+                                             tnr = 0, fpr = 1, fnr = 0, tpr = 1,
+                                             avg_overtime_pct = 0.10,
+                                             net_revenue_per_employee = 250000) {
+    
+    data_0_tbl <- as_tibble(data)
+    
+    
+    # 4. Expected Value 
+    
+    # 4.1 Calculating Expected Value With OT 
+    
+    pred_0_tbl <- h2o_model %>%
+        h2o.predict(newdata = as.h2o(data_0_tbl)) %>%
+        as_tibble() %>%
+        bind_cols(
+            data_0_tbl %>%
+                select(EmployeeNumber, MonthlyIncome, OverTime)
+        )
+    
+    ev_0_tbl <- pred_0_tbl %>%
+        mutate(
+            attrition_cost = calculate_attrition_cost(
+                n = 1,
+                salary = MonthlyIncome * 12,
+                # Changed in _2 ----
+                net_revenue_per_employee = net_revenue_per_employee) 
+        ) %>%
+        mutate(
+            cost_of_policy_change = 0
+        ) %>%
+        mutate(
+            expected_attrition_cost = 
+                Yes * (attrition_cost + cost_of_policy_change) +
+                No *  (cost_of_policy_change)
+        )
+    
+    
+    total_ev_0_tbl <- ev_0_tbl %>%
+        summarise(
+            total_expected_attrition_cost_0 = sum(expected_attrition_cost)
+        )
+    
+    # 4.2 Calculating Expected Value With Targeted OT
+    
+    data_1_tbl <- data_0_tbl %>%
+        add_column(Yes = pred_0_tbl$Yes) %>%
+        mutate(
+            OverTime = case_when(
+                Yes >= threshold ~ factor("No", levels = levels(data_0_tbl$OverTime)),
+                TRUE ~ OverTime
+            )
+        ) %>%
+        select(-Yes) 
+    
+    pred_1_tbl <- h2o_model %>%
+        h2o.predict(newdata = as.h2o(data_1_tbl)) %>%
+        as_tibble() %>%
+        bind_cols(
+            data_0_tbl %>%
+                select(EmployeeNumber, MonthlyIncome, OverTime),
+            data_1_tbl %>%
+                select(OverTime)
+        ) %>%
+        rename(
+            OverTime_0 = OverTime...6,
+            OverTime_1 = OverTime...7
+        )
+    
+    
+    avg_overtime_pct <- avg_overtime_pct # Changed in _2 ----
+    
+    ev_1_tbl <- pred_1_tbl %>%
+        mutate(
+            attrition_cost = calculate_attrition_cost(
+                n = 1,
+                salary = MonthlyIncome * 12,
+                # Changed in _2 ----
+                net_revenue_per_employee = net_revenue_per_employee)
+        ) %>%
+        mutate(
+            cost_of_policy_change = case_when(
+                OverTime_1 == "No" & OverTime_0 == "Yes" 
+                ~ attrition_cost * avg_overtime_pct,
+                TRUE ~ 0
+            )) %>%
+        mutate(
+            cb_tn = cost_of_policy_change,
+            cb_fp = cost_of_policy_change,
+            cb_fn = attrition_cost + cost_of_policy_change,
+            cb_tp = attrition_cost + cost_of_policy_change,
+            expected_attrition_cost = Yes * (tpr*cb_tp + fnr*cb_fn) + 
+                No * (tnr*cb_tn + fpr*cb_fp)
+        )
+    
+    
+    total_ev_1_tbl <- ev_1_tbl %>%
+        summarise(
+            total_expected_attrition_cost_1 = sum(expected_attrition_cost)
+        )
+    
+    
+    # 4.3 Savings Calculation
+    
+    savings_tbl <- bind_cols(
+        total_ev_0_tbl,
+        total_ev_1_tbl
+    ) %>%
+        mutate(
+            savings = total_expected_attrition_cost_0 - total_expected_attrition_cost_1,
+            pct_savings = savings / total_expected_attrition_cost_0
+        )
+    
+    return(savings_tbl$savings)
+    
+}
+
+test_tbl %>%
+    calculate_savings_by_threshold_2(automl_leader, 
+                                     avg_overtime_pct = 0.15, 
+                                     net_revenue_per_employee = 300000)
+
+
 # 6.2 Sensitivity Analysis ----
+
+# start by setting threshold to the new optimised for expected savings value
+max_savings_rates_tbl <- rates_by_threshold_optimised_tbl %>% 
+    filter(savings == max(savings))
+
+# CLASSIFIER CALIBRATION
+# this combination of threshold and expected rates settings has classifier 
+# calibrated to optimum FN / FP ratio for max savings 
+# NOTE: if cost/benefit change, the settings need to be recalibrated (re-optimised)
+
+max_savings_rates_tbl
+
+calculate_savings_by_threshold_2(
+    data = test_tbl,
+    h2o_model = automl_leader,
+    threshold = max_savings_rates_tbl$threshold,
+    tnr = max_savings_rates_tbl$tnr, 
+    fnr = max_savings_rates_tbl$fnr,
+    fpr = max_savings_rates_tbl$fpr,
+    tpr = max_savings_rates_tbl$tpr
+)
+
+# create pre-loaded function with purrr:partial function 
+
+calculate_savings_by_threshold_2_preloaded <- partial(
+    calculate_savings_by_threshold_2,
+    # function arguments
+    data = test_tbl,
+    h2o_model = automl_leader,
+    threshold = max_savings_rates_tbl$threshold,
+    tnr = max_savings_rates_tbl$tnr, 
+    fnr = max_savings_rates_tbl$fnr,
+    fpr = max_savings_rates_tbl$fpr,
+    tpr = max_savings_rates_tbl$tpr
+)
+
+calculate_savings_by_threshold_2_preloaded(avg_overtime_pct = 0.10, 
+                                           net_revenue_per_employee = 250000)
+# partial function simplifies our workflow so only need to include the arguments we want 
+# to iterate 
+
+# task: iterating over multiple combinations of two inputs above 
+
+# intuition 
+# -  over time. 100% would be doubling of an employees hours 
+#   - expect worst case to be 30% which is roughly 12 hour per week (if 40 hr week)
+# - net revenue - estimate range btw 200000 to 400000
+
+# create a list and use purrr::cross_df to create all combinations of each variable
+# useful for grid search and sensitivity analysis 
+
+sensitivity_tbl <- list(
+    avg_overtime_pct = seq(0.05, 0.30, by = 0.05),
+    net_revenue_per_employee = seq(200000, 400000, by = 50000)
+) %>% 
+    cross_df() %>% 
+    mutate(
+        savings = pmap_dbl(
+            .l = list(avg_overtime_pct = avg_overtime_pct, 
+                      net_revenue_per_employee = net_revenue_per_employee),
+            .f = calculate_savings_by_threshold_2_preloaded
+        )
+    )
+# pmap list maps list cols to fucntion args
+sensitivity_tbl
+
+sensitivity_tbl %>% 
+    ggplot(aes(avg_overtime_pct, net_revenue_per_employee)) + 
+    geom_tile(aes(fill = savings)) +
+    geom_label(aes(label = savings %>% round(0) %>% scales::dollar())) +
+    theme_tq() +
+    theme(legend.position = "none") +
+    scale_fill_gradient2(
+        low = palette_light()[[2]],
+        mid = "white",
+        high = palette_light()[[1]],
+        #midpoint = mean(sensitivity_tbl$savings)
+        midpoint = 0
+    ) +
+    scale_x_continuous(
+        labels = scales::percent,
+        breaks = seq(0.05, 0.30, by = 0.05)
+    ) + 
+    scale_y_continuous(labels = scales::dollar) +
+    labs(title = "Profitability Heatmap: Expected Savings Sensitivity Analysis",
+         subtitle = "How sensitive is savings to net revenue per employee and average overtime percentage?", 
+         x = "Average Overtime Percentage",
+         y = "Net Revenue Per Employee")
+
+
+# Explaining sensitivity analysis -----------------------------------------
+
+# Sensitivity analysis - purpose is accounting for unknowns
+
+# heatmap is a good way to show how sensitive two features are to expected ROI
+# 
+# note: for more variables it can be difficult to visualise - one thing to look at 
+# is a Tornado plot... search on stack overflow 
+
+# Looking at the plot; two inputs that are unknown or uncertain and grid shows 
+# how high, mid and low combinations interact to deliver different expected savings
+# 
+# Recap on variables: 
+# 
+# Net Revenue Per Employee
+# - Net Revenue = Revenue - COGS 
+# - NRPE = Net Revenue / No. of Employees
+# --  most likely = 250,000 
+# --  worst case = 200,000 
+# --  best case = 400,000
+# 
+# Average over time pct 
+# - Average OT % - (Hours worked - 40) / 40
+# - Higher values end up hurting this analysis as 
+# - Out break even pooint on average over time is btw 25 and 30% 
+# -- As long as people aren't working over 26%, we'd have savings benefits by reducing over time 
+# in line with model recommendations 
+
+
+
+
+
+
+
+
+
